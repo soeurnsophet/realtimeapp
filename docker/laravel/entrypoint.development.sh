9cb2ceefe99b2e0a7c2ae156ec75441f7d390dd4
soeurn sophet <soeurnsophet@gmail.com>
2026-09-14 20:32:14 +0700

    Project Setup.

---
+++ docker/laravel/entrypoint.development.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+composer install
+wait $!
+npm install
+wait $!
+php artisan key:generate
+wait $!
+php artisan migrate
+wait $!
+# php artisan storage:link
+# Start Reverb WebSocket server in background
+php artisan reverb:start --host=0.0.0.0 --port=8080 &
+
+# Start Laravel development server
+php artisan serve --host=0.0.0.0 --port=8000
---
+++ frontend/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router/index.js'
+import App from './App.vue'
+import './main.css'
+
+const app = createApp(App)
+
+const pinia = createPinia()
+app.use(router)
+app.use(pinia)
+app.mount('#app')
